620186783b9aecc8087ebb9191b94cb84761d79f
vipandeepkour <vipandeep.bbsbec@gmail.com>
2022-04-22 17:49:36 +0530

    Polymorphism: Method overloading

---
+++ Mainpolyclass.java
@@ -0,0 +1,12 @@
+package poly_package;
+
+
+public class Mainpolyclass {
+
+	public static void main(String[] args) 
+	{
+		System.out.println(Otherpolyclass.Multiply(2,3));
+		System.out.println(Otherpolyclass.addition(10.58925, 45.25542));
+	}
+
+}
---
+++ Otherpolyclass.java
@@ -0,0 +1,13 @@
+package poly_package;
+
+public class Otherpolyclass //same method name, different parameters, irrespective of return type
+{
+static int Multiply(int a,int b)//
+{
+	return a*b;
+}
+static double addition (double a,double b )
+{
+	return a+b;
+}
+}
